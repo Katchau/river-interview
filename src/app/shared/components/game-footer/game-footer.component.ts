45f09212aafabdd61034ab9b8624e02358451ef4
Joao <joaohccl@hotmail.com>
2022-05-24 05:11:29 +0100

    add: updated grid layout, added basic footer
    fixed issue with zooming

--- src/app/shared/components/game-footer/game-footer.component.ts
+++ src/app/shared/components/game-footer/game-footer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Game } from '../../client/game.model';
 
 @Component({
   selector: 'app-game-footer',
   templateUrl: './game-footer.component.html',
-  styleUrls: ['./game-footer.component.scss']
+  styleUrls: ['./game-footer.component.scss'],
+	changeDetection: ChangeDetectionStrategy.Default,
 })
-export class GameFooterComponent implements OnInit {
+export class GameFooterComponent implements OnInit, OnChanges {
 
+  @Input()
+  public recentGames: Game[] = [];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }
